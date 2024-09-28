3e18acdae244ea8628758d42dd54fb94014ca35e
Dev_Faruq <olatoyesefaruq@gmail.com>
2024-09-28 05:20:43 +0100

    Building full autheticated E-COMMERCE with reactjs udemy course

---
+++ src/component/categories/categories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './categories.style.scss'
+import CategoryItem from '../category-item/CategoryItem';
+
+
+const Categories = ({categories})=> {
+  return (
+    <div className="categories-container">
+    {
+      categories.map((category)=>(
+       <CategoryItem key={category.id}  category={category} />
+      ))
+    }
+  </div>
+  )
+}
+
+export default Categories
