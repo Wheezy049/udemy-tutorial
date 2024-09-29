4f5de0422f67d8724ffc3962cf124852fcbdc076
Dev_Faruq <olatoyesefaruq@gmail.com>
2024-09-29 08:17:13 +0100

    worked on react-router-dom

---
+++ src/pages/header/Header.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import { ReactComponent as CrownLogo} from '../../image/crown.svg'
+import './header.scss'
+
+const Header = () =>{
+  return (
+    // fragment or <>
+    <Fragment> 
+        <div className='navigation'>
+            <Link to='/' className='logo-container'>
+               <CrownLogo className="logo"/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link to='/shop' className='nav-link'>Shop</Link>
+            </div>
+        </div>
+        <Outlet />
+    </Fragment>
+  )
+}
+
+export default Header
